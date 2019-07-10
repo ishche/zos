6649e1180523c058754f5657c5ba42bb554a713d
Iurii Shchekochikhin <iurii.shchekochikhin@broadcom.com>
2019-07-10 14:46:18 +0200

    cleanup code and add loading datasets from folder

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,27 @@
+import * as fs from "fs";
 import * as jserver from "json-server";
+import * as path from "path";
 
 const server = jserver.create();
 const middlewares = jserver.defaults();
 server.use(middlewares);
 
 server.get("/api/v1/datasets/username", (req, res) => {
-  res.send({ username: "HELOKITY" });
+    res.send({ username: "HELOKITY" });
 });
 
 server.get("/api/v1/datasets/*/list", (req, res) => {
-  const filter = req.params[0];
-  res.send(generateDatasets(filter));
+    const filter = req.params[0];
+    res.send(generateDatasets(filter));
 });
 
 server.get("/api/v1/datasets/*", (req, res) => {
-  const filter = req.params[0];
-  res.send(generateDatasets(filter));
+    const filter = req.params[0];
+    res.send(generateDatasets(filter));
 });
 
 server.get("/api/v1/datasets/*/members", (req, res) => {
-    const dataSetName = req.params[0];
-    res.send({"items": ["MEMBER1", "MEMBER2", "MEMBER3"]});
+    res.send({ items: ["MEMBER1", "MEMBER2", "MEMBER3"] });
 });
 
 const router = jserver.router("db.json");
@@ -28,20 +29,33 @@ server.use(router);
 
 const port: number = 8443;
 server.listen(port, () => {
-  console.log("Zowe data-sets mock api server is running at " + port);
+    // tslint:disable-next-line: no-console
+    console.log("Zowe data-sets mock api server is running at " + port);
 });
 
 function generateDatasets(filter: string) {
-  return {
-    items: [
-      {
-        migrated: false,
-        name: filter.split("*").join("A").toLocaleUpperCase()
-      }
-    ]
-  };
-}
+    const dataFolder = path.join("dataset");
+    if (!fs.existsSync(dataFolder)) {
+        fs.mkdirSync(dataFolder);
+    }
 
-interface Dataset {
-  name: String;
+    const datasets = fs.readdirSync(dataFolder);
+    const result = [];
+    if (datasets.length > 0) {
+        for (const dataset of datasets) {
+            result.push({
+                migrated: false,
+                name: dataset,
+            });
+        }
+    } else {
+        result.push({
+            migrated: false,
+            name: filter
+                .split("*")
+                .join("A")
+                .toLocaleUpperCase(),
+        });
+    }
+    return { items: result };
 }
